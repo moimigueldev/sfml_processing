544645d2ee4eda98aded9649b4acbef3ab4eda02
Moises Miguel <moimigueldev@gmail.com>
2023-09-26 15:29:47 -0500

    maze finds path

--- collection/maze_generator/sketch.js
+++ collection/maze_generator/sketch.js
@@ -30,9 +30,12 @@ function draw() {
   }
 
   current.visited = true;
+  current.highlight();
   let next = current.checkNeighbors();
   if (next) {
     next.visited = true;
+
+    removeWalls(current, next);
     current = next;
   }
 }
@@ -86,6 +89,14 @@ function Cell(i, j) {
     return i + j * cols;
   }
 
+  this.highlight = function () {
+    let x = this.i * w;
+    let y = this.j * w;
+    noStroke();
+    fill(0, 0, 255, 100);
+    rect(x, y, w, w);
+  };
+
   this.show = function () {
     let x = this.i * w;
     let y = this.j * w;
@@ -117,4 +128,24 @@ function Cell(i, j) {
       rect(x, y, w, w);
     }
   };
+}
+
+function removeWalls(a, b) {
+  let x = a.i - b.i;
+  if (x === 1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  } else if (x === -1) {
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+
+  let y = a.j - b.j;
+  if (y === 1) {
+    a.walls[0] = false;
+    b.walls[2] = false;
+  } else if (y === -1) {
+    a.walls[2] = false;
+    b.walls[0] = false;
+  }
 }
