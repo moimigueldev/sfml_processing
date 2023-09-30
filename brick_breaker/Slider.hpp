b0588ca4003331b36c0b8889714b1361551e386e
Moises Miguel <moimigueldev@gmail.com>
2023-09-30 11:23:42 -0500

    implemented slider

---
+++ brick_breaker/Slider.hpp
@@ -0,0 +1,81 @@
+#pragma once
+#include <iostream>
+#include<SFML/Graphics.hpp>
+
+class Slider {
+  private:
+    sf::Vector2f sliderSize = sf::Vector2f(200.0f, 15.0f);
+    sf::Vector2f vel;
+    sf::Vector2f acc;
+
+    const float MAX_VEL = 40.0f;
+
+  public:
+    sf::RectangleShape slider;
+    Slider(sf::Vector2f pos) {
+
+
+
+
+
+      slider.setPosition(pos.x, pos.y);
+      slider.setSize(sliderSize);
+      slider.setOrigin(sliderSize.x / 2, sliderSize.y / 2);
+      slider.setFillColor(sf::Color::White);
+    }
+
+
+    void update(sf::Vector2f windowSize) {
+      sf::Vector2f position = slider.getPosition();
+
+      vel+=acc;
+
+      // if(vel.x > MAX_VEL) {
+      //   vel.x = MAX_VEL;
+      // }
+
+
+
+      slider.move(vel);
+
+    
+      checkPosition(windowSize, position);
+      
+
+      acc *= 0.9f;
+      vel *= 0.9f;
+
+    }
+
+    void draw(sf::RenderWindow& window) {
+      window.draw(slider);
+
+    }
+
+
+    void checkPosition(sf::Vector2f windowSize, sf::Vector2f position) {
+      if(position.x - sliderSize.x / 2 < 0) {
+        nullVelAcc();
+        slider.setPosition(sliderSize.x / 2, position.y);
+      }
+
+      if(position.x + sliderSize.x / 2 > windowSize.x) {
+        nullVelAcc();
+        slider.setPosition(windowSize.x - sliderSize.x / 2, position.y);
+      }
+    }
+
+
+    void addAcc(sf::Vector2f force) {
+      acc += force;
+    }
+
+
+    void nullVelAcc() {
+      vel.x = 0.0f;
+      acc *= 0.0f;
+ 
+    }
+
+};
+
---
+++ brick_breaker/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Slider.hpp"
+
+sf::Vector2f windowSize = sf::Vector2f(1500.0f, 800.0f);
+sf::Color bgColor = sf::Color(68, 70, 83);
+const int FRAMERATE = 60;
+Slider slider = Slider(sf::Vector2f( windowSize.x / 2, windowSize.y - 100 ));
+
+
+// Components:
+    // Bottom slider
+    // The ball
+    // Walls (different color per row)
+
+
+int main() {
+
+  sf::RenderWindow window(sf::VideoMode(windowSize.x, windowSize.y), "SFML");
+  window.setFramerateLimit(FRAMERATE);
+
+  // Main Loop
+  while(window.isOpen()) {
+    sf::Event event;
+
+    if(window.pollEvent(event)) {
+    
+      if(event.type == sf::Event::Closed) {
+        window.close();
+      }
+
+     switch (event.key.code) {
+        case sf::Keyboard::Left:
+          slider.addAcc(sf::Vector2f(-1.5f, 0.0f));
+          break;
+        case sf::Keyboard::Right:
+          slider.addAcc(sf::Vector2f(1.5f, 0.0f));
+          break;
+        default:
+          break;
+          
+      }
+
+    }
+
+
+    window.clear(bgColor);
+
+
+    slider.update(windowSize);
+    slider.draw(window);
+
+    window.display();
+
+  }
+
+  return 0;
+}
