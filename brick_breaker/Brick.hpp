c1563c8f7004769e7f43fb60e467c5be32dc6459
Moises Miguel <moimigueldev@gmail.com>
2023-09-30 18:22:41 -0500

    implemented bricks

---
+++ brick_breaker/Brick.hpp
@@ -0,0 +1,85 @@
+#pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Ball.hpp"
+#include <array>
+
+class Brick {
+  private:
+    sf::Vector2f brickSize = sf::Vector2f(80.0f, 15.0f);
+
+
+  public:
+    sf::RectangleShape brick;
+    Brick(sf::Vector2f pos) {
+      brick.setPosition(pos.x, pos.y);
+      brick.setSize(brickSize);
+      brick.setOrigin(brickSize.x / 2, brickSize.y / 2);
+      brick.setFillColor(sf::Color::Blue);
+    }
+
+
+  
+
+    void draw(sf::RenderWindow& window) {
+      window.draw(brick);
+    }
+
+
+
+    bool checkBallCollision(Ball& ball) {
+      sf::Vector2f ballPosition = ball.ball.getPosition();
+      sf::Vector2f brickPos = brick.getPosition();
+      int radius = ball.RADIUS;
+
+
+     // Top Edge
+      if (ballPosition.y + radius > brickPos.y - brickSize.y / 2 && 
+          ballPosition.y + radius < brickPos.y &&
+          ballPosition.x + radius > brickPos.x - brickSize.x / 2 &&
+          ballPosition.x - radius < brickPos.x + brickSize.x / 2) {
+          std::cout << "Collision with top edge!" << std::endl;
+          ball.vel.y *= -1;
+          return true;
+      }
+
+      // Left Edge
+      if (ballPosition.x + radius > brickPos.x - brickSize.x / 2 && 
+          ballPosition.x + radius < brickPos.x &&
+          ballPosition.y + radius > brickPos.y - brickSize.y / 2 &&
+          ballPosition.y - radius < brickPos.y + brickSize.y / 2) {
+          std::cout << "Collision with left edge!" << std::endl;
+          ball.ball.setPosition(brickPos.x - brickSize.x / 2 - radius, ballPosition.y);
+          ball.vel.x *= -1;
+          return true;
+      }
+
+      // Right Edge
+      if (ballPosition.x - radius < brickPos.x + brickSize.x / 2 && 
+          ballPosition.x - radius > brickPos.x &&
+          ballPosition.y + radius > brickPos.y - brickSize.y / 2 &&
+          ballPosition.y - radius < brickPos.y + brickSize.y / 2) {
+          std::cout << "Collision with right edge!" << std::endl;
+          ball.vel.x *= -1;
+          return true;
+      }
+
+     
+
+      // Bottom Edge (unlikely in a brick breaker game, but included for completeness)
+      if (ballPosition.y - radius < brickPos.y + brickSize.y / 2 && 
+          ballPosition.y - radius > brickPos.y &&
+          ballPosition.x + radius > brickPos.x - brickSize.x / 2 &&
+          ballPosition.x - radius < brickPos.x + brickSize.x / 2) {
+          std::cout << "Collision with bottom edge!" << std::endl;
+          ball.vel.y *= -1;
+          return true;
+      }
+
+      return false;
+}
+
+
+};
+
+
--- brick_breaker/main.cpp
+++ brick_breaker/main.cpp
@@ -2,13 +2,22 @@
 #include <SFML/Graphics.hpp>
 #include "Slider.hpp"
 #include "Ball.hpp"
+#include "Brick.hpp"
 
 sf::Vector2f windowSize = sf::Vector2f(1500.0f, 800.0f);
 sf::Color bgColor = sf::Color(68, 70, 83);
 const int FRAMERATE = 60;
 Slider slider = Slider(sf::Vector2f( windowSize.x / 2, windowSize.y - 100 ));
 Ball ball = Ball(sf::Vector2f( windowSize.x / 2, windowSize.y / 2 ));
+Brick brick = Brick(sf::Vector2f( windowSize.x / 2, windowSize.y / 2 - 200 ));
 
+
+sf::Vector2f brickSize = sf::Vector2f(80.0f, 15.0f);
+
+const float PADDING = 10.0f; // Define a padding value
+
+int cols = (windowSize.x - PADDING) / (brickSize.x + PADDING);
+int rows = (windowSize.y / 2 - PADDING) / (brickSize.y + PADDING);
 
 // Components:
     // Bottom slider
@@ -17,6 +26,18 @@ Ball ball = Ball(sf::Vector2f( windowSize.x / 2, windowSize.y / 2 ));
 
 
 int main() {
+
+
+  std::vector<Brick> bricks;
+
+
+ for (int j = 0; j < rows; j++) {
+    for (int i = 0; i < cols; i++) {
+        float x = PADDING + i * (brickSize.x + PADDING);
+        float y = PADDING + j * (brickSize.y + PADDING);
+        bricks.push_back(Brick(sf::Vector2f(x, y)));
+    }
+}
 
   sf::RenderWindow window(sf::VideoMode(windowSize.x, windowSize.y), "SFML");
   window.setFramerateLimit(FRAMERATE);
@@ -33,10 +54,10 @@ int main() {
 
      switch (event.key.code) {
         case sf::Keyboard::Left:
-          slider.addAcc(sf::Vector2f(-2.0f, 0.0f));
+          slider.addAcc(sf::Vector2f(-1.0f, 0.0f));
           break;
         case sf::Keyboard::Right:
-          slider.addAcc(sf::Vector2f(2.0f, 0.0f));
+          slider.addAcc(sf::Vector2f(1.0f, 0.0f));
           break;
         default:
           break;
@@ -49,7 +70,13 @@ int main() {
     window.clear(bgColor);
 
 
- slider.checkBallCollision(ball);
+     slider.checkBallCollision(ball);
+
+     for (int i = 0; i < bricks.size(); i++) {
+       bricks[i].draw(window);
+     }
+
+
     ball.update(windowSize);
     ball.draw(window);
 
