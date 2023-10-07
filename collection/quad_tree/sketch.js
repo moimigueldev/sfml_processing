9b33e41e41bb961d9872de6c9aee51d547e1cd82
Moises Miguel <moimigueldev@gmail.com>
2023-10-06 20:15:59 -0500

    completed part 3 of quad tree

--- collection/quad_tree/sketch.js
+++ collection/quad_tree/sketch.js
@@ -1,46 +1,55 @@
-let qtree;
-let count = 100;
+let particles = [];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 
-  let boundary = new Rectangle(200, 200, 200, 200);
-  qtree = new QuadTree(boundary, 4);
-
-  for (let index = 0; index < 200; index++) {
-    let p = new Point(random(width), random(height));
-    qtree.insert(p);
+  for (let index = 0; index < 1000; index++) {
+    particles[index] = new Particle(random(width), random(height));
   }
-  console.log("qtree", qtree);
 }
 
 function draw() {
-  // put drawing code here
+  background(0);
 
-  if (mouseIsPressed) {
-    console.log("click");
-    for (let index = 0; index < 5; index++) {
-      let m = new Point(mouseX + random(-5, 5), mouseY + random(-5, 5));
-      qtree.insert(m);
+  let boundary = new Rectangle(300, 200, 600, 400);
+  qtree = new QuadTree(boundary, 4);
+
+  // draw Boundary
+  // stroke(255, 0, 0);
+  // noFill();
+  // strokeWeight(4);
+  // rectMode(CENTER);
+  // rect(300, 200, 600, 400);
+
+  for (let index = 0; index < particles.length; index++) {
+    let point = new Point(
+      particles[index].x,
+      particles[index].y,
+      particles[index]
+    );
+    qtree.insert(point);
+    particles[index].move();
+    particles[index].render();
+    particles[index].setHighlight(false);
+  }
+
+  for (let p of particles) {
+    // circle is the range
+    let range = new Circle(p.x, p.y, p.r * 2);
+
+    // draw range
+    // stroke(0, 255, 0);
+    // strokeWeight(1);
+    // ellipse(p.x, p.y, p.r * 2); // Draw a circle at (200, 200) with a diameter of 50 pixels
+    //
+    let points = qtree.query(range);
+    for (let point of points) {
+      let other = point.userData;
+      if (p !== other && p.intersects(other)) {
+        p.setHighlight(true);
+      }
     }
   }
 
-  background(0);
   qtree.show();
-
-  stroke(0, 255, 0);
-  rectMode(CENTER);
-  let range = new Rectangle(mouseX, mouseY, 25, 25);
-  rect(range.x, range.y, range.w * 2, range.h * 2);
-
-  let points = qtree.query(range, count);
-
-  for (let p of points) {
-    strokeWeight(4);
-    point(p.x, p.y);
-  }
-
-  // console.log(count);
-
-  // noLoop();
 }
